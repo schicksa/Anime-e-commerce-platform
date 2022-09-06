45358bb83609038b43ff58b02bdedc6e22082a26
schicksa <101779598+schicksa@users.noreply.github.com>
2022-09-06 11:09:44 +0800

    Update api_constant.js

--- src/api/api_constant.js
+++ src/api/api_constant.js
@@ -2,8 +2,30 @@ export const BASE_URL = "http://localhost:3000";
 export const JWT_VALIDATE_URL = "http://localhost:3001";
 export const TIMEOUT = 5000;
 
-
-// Anime image data
+// GOODS list API address GET
+export const GOODS_CATEGORY_API = "/goods"; //None
+// Animation image data
 export const HOME_CARTOON_LIST = "/cartoon"
-// Carousel data
+// Rotary-cast graph data
 export const BANNERS = "/banner"
+// Add commodity POST
+export const ADD_COMMDITY = "/commdity"
+// Acquire COMMDITY list GET
+export const GET_COMMDITY_LIST = "/commdity"
+// DELETE COMMDITY
+export const DELETE_COMMDITY  = "/commdity"
+// Find an COMMDITY based on its ID  GET
+export const COMMDITY_BY_ID = "/commdity"
+// Add shopping cart data to the database POST
+export const ADD_CART = "/cart"
+// Add to favourites  POST
+export const ADD_FAVORITE = "/favorites"
+// user register POST
+export const REGISTER_USER = "/users"
+// login GET
+export const LOGIN = "/users"
+
+// Generate JWT
+export const GEN_TOKEN = "/generateToken"
+// JWT validation
+export const AUTH = "/auth"
