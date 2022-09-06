cabee415d3109269e9a20ae595a849408c6cbee1
schicksa <101779598+schicksa@users.noreply.github.com>
2022-09-06 11:08:57 +0800

    Update server.js

--- server/server.js
+++ server/server.js
@@ -1,6 +1,13 @@
 const express = require("express");
+const cors = require("cors")
 const app = express();
+const userRouter = require("./router/userRouter")
 const config = require("./config/config")
 
+app.use(cors());
+app.use(express.json()) // can parse application/json
+app.use(express.urlencoded({ extended: true })) // can parse application/x-www-form-urlencoded
+
+app.use(userRouter);
 
 app.listen(config.port);
