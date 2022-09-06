47a9661b8b4de2892d4fee999edbb464904c44fa
schicksa <101779598+schicksa@users.noreply.github.com>
2022-09-06 11:07:26 +0800

    Update index.js

--- src/router/index.js
+++ src/router/index.js
@@ -1,13 +1,40 @@
 import Vue from "vue";
 import vueRouter from "vue-router";
 
-//  Home
+// First-level routing Home
 import Home from "@/pages/Home";
 
-// Banner
+// First-level routing Search
+import Search from "@/pages/Search"; //None
+
+// First-level routing Login
+import Login from "@/pages/Login";
+
+// First-level routing Register
+import Register from "@/pages/Register";
+
+// First-level routing
+import Goods from "@/pages/Goods"; //None
+
+// First-level routing
 import Banner from "@/pages/Banner";
 
-// 404
+// First-level routing
+import Cart from "@/pages/Cart";
+
+// First-level routing
+import Commdity from "@/pages/Commodity";
+
+// First-level routing
+import Magnifer from "@/pages/Magnifier"; //None
+
+// First-level routing
+import Favorites from "@/pages/Favorites";
+
+// First-level routing
+import Profile from "@/pages/Profile"
+
+// First-level routing 404
 import NotFound from "@/pages/NotFound";
 
 Vue.use(vueRouter);
@@ -26,10 +53,62 @@ const router = new vueRouter({
       meta: { isShowFooter: true },
     },
     {
+      path: "/search",
+      name: "search",
+      component: Search,
+      meta: { isShowFooter: true },
+    },
+    {
+      path: "/register",
+      name: "register",
+      component: Register,
+    },
+    {
+      path: "/login",
+      name: "login",
+      component: Login,
+    },
+    {
+      path: "/goods/:id?",
+      name: "goods",
+      component: Goods,
+      meta: { isShowFooter: true },
+    },
+    {
       path: "/banner",
       name: "banner",
       component: Banner,
       meta: { isShowFooter: true },
+    },
+    {
+      path: "/cart",
+      name: "cart",
+      component: Cart,
+      meta: { isShowFooter: true ,isAuth:true },
+    },
+    {
+      path: "/commdity",
+      name: "commdity",
+      component: Commdity,
+      meta: { isShowFooter: true },
+    },
+    {
+      path: "/magnifer",
+      name: "magnifer",
+      component: Magnifer,
+      meta: { isShowFooter: true },
+    },
+    {
+      path:"/favorites",
+      name:"favorites",
+      component:Favorites,
+      meta: { isShowFooter: true ,isAuth:true}
+    },
+    {
+      path:"/profile",
+      name:"profile",
+      component:Profile,
+      meta: { isShowFooter: true ,isAuth:true }
     },
     {
       path: "*",
