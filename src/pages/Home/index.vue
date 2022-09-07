0ef954da270fb132e0967601bacbb1b4799132aa
schicksa <101779598+schicksa@users.noreply.github.com>
2022-09-07 13:07:13 +0800

    font color

--- src/pages/Home/index.vue
+++ src/pages/Home/index.vue
@@ -12,6 +12,9 @@
       >Return to search page</a
     >
     <div id="home">
+      <div class="title">
+        <h1><span style="color: crimson;">ACGN </span> Homepage</h1>
+      </div>
       <template v-if="isShow">
         <div v-for="item in listData" :key="item.id" class="home-panel">
           <div class="home-panel-wrapper">
@@ -49,7 +52,7 @@ export default {
     NoData,
     Loading,
   },
-  mounted() {
+  mounted(){
     // Show cartoon list
     getAllCartoonListData(this);
   },
@@ -67,10 +70,38 @@ export default {
 </script>
 
 <style lang="less" scoped>
+.title{
+  position: absolute;
+  top:55%;
+  left:50%;
+  transform: translate(-50%,-50%);
+  //z-index: -1;
+  h1{
+    color: #fff;
+    font-size: 70px;
+    font-family: Century Gothic;
+  }
+}
+.home-panel-title{
+  color: #f35626;
+  background: coral;
+  background-image: -webkit-linear-gradient(45deg,#f35626,#feab3a);
+  -webkit-background-clip: text;
+  -webkit-text-fill-color: transparent;
+  -webkit-animation: hue 6s infinite linear;
+}
+@-webkit-keyframes hue {
+  from {
+    -webkit-filter: hue-rotate(0deg);
+  }
+  to {
+    -webkit-filter: hue-rotate(-360deg);
+  }
+}
 .back {
   padding-left: 15px;
   font-size: 14px;
-  color: #333;
+  color: #fff;
 }
 
 #home {
@@ -78,15 +109,19 @@ export default {
   height: auto;
   justify-content: center;
   flex-wrap: wrap;
+
   .home-panel {
     width: 25%;
     margin-bottom: 15px;
     padding: 10px;
     height: 320px;
     overflow: hidden;
+
     &-title {
       margin: 5px 0;
+      font-size: 15px;
     }
+
     &-wrapper {
       width: 100%;
       height: 270px;
@@ -94,38 +129,52 @@ export default {
       overflow: hidden;
       border: 1px solid #ddd;
       cursor: pointer;
+
       img {
-        transition: 0.3s;
+        transition: 0.5s;
         width: 100%;
         height: 100%;
+        filter: alpha(opacity=80);
+        -moz-opacity: 0.8;
+        opacity: 0.8;
+
         &:hover {
-          transform: scale(1.3) skew(10deg);
+          transform: scale(1.1) skew(4deg);
+          filter: brightness(110%);
+          opacity: 1;
+          transition: 0.3s;
+          width: 100%;
+          height: 100%;
+
+          &:hover {
+            transform: scale(1.3) skew(10deg);
+          }
         }
       }
     }
   }
-}
 
-@media screen and (max-width: 998px) {
-  #home {
-    .home-panel {
-      width: 33.3%;
+  @media screen and (max-width: 998px) {
+    #home {
+      .home-panel {
+        width: 33.3%;
+      }
     }
   }
-}
 
-@media screen and (max-width: 760px) {
-  #home {
-    .home-panel {
-      width: 50%;
+  @media screen and (max-width: 760px) {
+    #home {
+      .home-panel {
+        width: 50%;
+      }
     }
   }
-}
 
-@media screen and (max-width: 460px) {
-  #home {
-    .home-panel {
-      width: 100%;
+  @media screen and (max-width: 460px) {
+    #home {
+      .home-panel {
+        width: 100%;
+      }
     }
   }
 }
